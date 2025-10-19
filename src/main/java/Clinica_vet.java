ca5df0a990fc8043ac7d4be71f546756ea6291a1
EljoseXD <eljotas13xd@gmail.com>
2025-10-19 18:04:51 -0500

    first commit

---
+++ src/main/java/Clinica_vet.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author eljot
+ */
+public class Clinica_vet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
